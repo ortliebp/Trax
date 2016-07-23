01114bd1b3947b697a1bd7c1ab2a90b4a0e1ff8f
David Crawford <crawford@mail.gvsu.edu>
2016-07-23 17:19:23 -0400

    Working on switching activity view changes to Intents.

--- Trax/MainActivity.cs
+++ Trax/MainActivity.cs
@@ -1,24 +1,22 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
-namespace Trax
-{
+namespace Trax {
 	[Activity(Label = "Trax", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
-	{
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
+	public class MainActivity : Activity {
+		protected override void OnCreate(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
-
 			SetContentView(Resource.Layout.Main);
+			//Intent whoAreYou = new Intent(this, typeof(whoAreYouPrompt));
 
 			Button settings = FindViewById<Button>(Resource.Id.settingsButton);
 			settings.Click += delegate {
-				StartActivity(typeof(whoAreYourPrompt));
+				Intent whoAreYou = new Intent(this, typeof(Trax.whoAreYouPrompt));
+				whoAreYou.PutExtra("MyData", "Data from Activity1");
+				StartActivity(whoAreYou);
 			};
 		}
 	}
 }
-
-
